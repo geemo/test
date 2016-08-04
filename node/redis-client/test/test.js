8856ced8b55d632eb91b7a5eb0a0101d39c51831
geemo <153330685@qq.com>
2016-08-04 20:53:42 +0800

    增强错误处理

--- node/redis-client/test/test.js
+++ node/redis-client/test/test.js
@@ -4,19 +4,37 @@ const redis = new Redis();
 
 redis.on('connect', () => {
     console.log('connected!');
-});
 
-redis.query('auth Wmeiyoumima')
-    .query('lrange list1 0 -1', (err, data) => {
-        if (err) console.log(err);
-        else console.log(data);
-    })
-    .query('set aa 5', (err, data) => {
-        if (err) console.log(err);
-        else console.log(data);
-    })
-    .query('get aa', (err, data) => {
-        if(err) console.log(err);
-        else console.log(data);
-        redis.close();
-    });
+// redis.query('auth Wmeiyoumima')
+//     .query('lrange list1 0 -1', (err, data) => {
+//         if (err) console.log(err);
+//         else console.log(data);
+//     })
+//     .query('set aa 5', (err, data) => {
+//         if (err) console.log(err);
+//         else console.log(data);
+//     })
+//     .query('get aa', (err, data) => {
+//         if(err) console.log(err);
+//         else console.log(data);
+//         redis.close();
+//     });
+
+
+    redis.auth('Wmeiyoumima')
+        .get('aa', (err, data) => {
+            if (err) return console.error(err);
+
+            console.log(data);
+            redis.close();
+            redis.set(['geemo', 'dog'], (err, result) => {
+                if(err) return console.error(err);
+
+                console.log(data);
+            });
+        });
+}).on('close', () => {
+    console.log('close');
+}).on('error', (err) => {
+    console.error(err)
+});
