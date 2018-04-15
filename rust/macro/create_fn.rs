016e59213cdb6113dfc233bdb8727530db2a4adc
geemo <153330685@qq.com>
2018-04-15 09:52:25 +0800

    lang(rust): add create_fn macro demo

---
+++ rust/macro/create_fn.rs
@@ -0,0 +1,15 @@
+macro_rules! create_fn {
+  ($fn_name:ident) => {
+    fn $fn_name() {
+      println!("You called {:?}()", stringify!($fn_name))
+    }
+  }
+}
+
+create_fn!(foo);
+create_fn!(bar);
+
+fn main() {
+  foo();
+  bar();
+}
